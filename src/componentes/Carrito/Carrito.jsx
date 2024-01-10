eea7703847fac7dcaf21adbef6743cccef78c05d
CristianJR <104873760+CJRUIZ-WEBDESIGN@users.noreply.github.com>
2024-01-10 15:47:54 -0300

    Se incorporo FireBase en el proyecto, se mejoro la vista del carrito, se incorporo CartContext, productos cargados correctamente en ItemListContainer.

--- src/componentes/Carrito/Carrito.jsx
+++ src/componentes/Carrito/Carrito.jsx
@@ -1,18 +1,31 @@
 import { useContext } from "react";
 import { CartContext } from "../CartContext/CartContext";
 import { FaTrashAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import "./Carrito.scss";
 
 const Carrito = () => {
-  const { carrito, borrarCarrito, borrarProducto } = useContext(CartContext);
+  const { carrito, borrarCarrito, borrarProducto, totalPrecio } =
+    useContext(CartContext);
+
+  if (carrito.length === 0) {
+    return (
+      <article className="carritovacio">
+        <h2>El carrito se encuentra vacio</h2>
+        <Link to="/" className="buttons">
+         <b>VOLVER AL INICIO</b>
+        </Link>
+      </article>
+    );
+  }
 
   return (
     <section id="carritoContainer">
       <ul className="carritoItem">
         {carrito.map((producto) => (
           <li key={producto.id}>
-            <img 
+            <img
               src={producto.img}
               alt={producto.nombre}
               style={{ width: "120px", height: "120px" }}
@@ -21,7 +34,7 @@ const Carrito = () => {
               <b>ID: </b> {producto.id}
             </p>
             <p>
-              <b>NOMBRE: </b> {producto.nombre}
+              <b>PRODUCTO: </b> {producto.nombre}
             </p>
             <p>
               <b>PRECIO: </b>$ {producto.precio}
@@ -38,8 +51,15 @@ const Carrito = () => {
           </li>
         ))}
       </ul>
+    
+      <article className="totalprecio">
+        <h3>TOTAL: {totalPrecio()}</h3>
+      </article>
 
-      <button className="buttons" onClick={borrarCarrito}><b>VACIAR CARRITO</b></button>
+        <button className="buttons" onClick={borrarCarrito}>
+        <b>VACIAR CARRITO</b>
+      </button>
+
     </section>
   );
 };
